ef3f9ccc9aaac233a1dcfd03f9eb234797c93c80
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2026-03-08 11:05:39 +0700

    feat(fck-bug): add 'Cũng là fix bug mà lạ lắm' - bug evasion game with gaslighting
    - Bug runs away when cursor is close (150px)
    - Bug teleports when about to be clicked (50px)
    - Fake loading screen (99%) then creates 100+ new bugs
    - VS Code background with line deletion
    - Gaslighting quotes + evil laugh on miss
    - Mobile responsive, no scroll issues
    - Intro screen doesn't reveal the game secrets

---
+++ src/views/fck-bug/meta.ts
@@ -0,0 +1,9 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Cũng là fix bug mà lạ lắm',
+  description: 'Thử cảm giác "fix bug" khi con bug không bao giờ chịu để bạn đập trúng!',
+  author: 'Nanoka',
+}
+
+export default meta
